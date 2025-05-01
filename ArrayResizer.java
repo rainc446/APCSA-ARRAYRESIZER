69778b14b1aa0349e7cbc6175c815e1d2780823a
rainy1221 <rain102208@gmail.com>
2025-05-01 18:48:54 -0400

    question a done

--- ArrayResizer.java
+++ ArrayResizer.java
@@ -3,18 +3,38 @@ public class ArrayResizer {
      * Precondition: r is a valid row index in array2D.
      * Postcondition: array2D is unchanged.
      */
+
+
+
     public static boolean isNonZeroRow(int[][] array2D, int r)
-    { /* to be implemented in part (a) */ }
+    { /* to be implemented in part (a) */
+        for (int c = 0; c < array2D[r].length; c++) {
+            if (array2D[r][c] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     /** Returns the number of rows in array2D that contain all non-zero values.
      * Postcondition: array2D is unchanged.
      */
     public static int numNonZeroRows(int[][] array2D)
-    { /* implementation not shown */ }
+    { /* implementation not shown */
+        int count = 0;
+        for (int r = 0; r < array2D[r].length; r ++) {
+            if (isNonZeroRow(array2D, r)) {
+                count ++;
+            }
+        }
+        return count;
+    }
     /** Returns a new, possibly smaller, two-dimensional array that contains only rows
      * from array2D with no zeros, as described in part (b).
      * Precondition: array2D contains at least one column and at least one row with no zeros.
      * Postcondition: array2D is unchanged.
      */
     public static int[][] resize(int[][] array2D)
-    { /* to be implemented in part (b) */ }
+    { /* to be implemented in part (b) */
+        return null;
+    }
 }
